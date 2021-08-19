ec9a70a5c232d11bfc0f9917f4b1413bf3a44541
Matveev Anton <reido@mail.ru>
2021-08-19 15:29:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = n.toString();
+    let deletesymbol = str.split('-').join('');
+    let reverse = deletesymbol.split('').reverse().join('');
+    return reverse;
 }
